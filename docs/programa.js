8ebac80a2662d0cda4db7023bfad4362b3eeb01b
Adriana Milena Toro Villalobos <torovillalobosmilena@gmail.com>
2025-05-12 15:54:33 -0500

    v.02v.01

--- docs/programa.js
+++ docs/programa.js
@@ -1,10 +1,10 @@
-var map = L.map('map').setView([51.505, -0.09], 13)
+var map = L.map('map').setView([4.709532764012459, -74.07180212149238], 13)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([4.709532764012459, -74.07180212149238]).addTo(map);
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
