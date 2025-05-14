f8100c695de116d5b950107423cd651b71f91867
Adriana Milena Toro Villalobos <torovillalobosmilena@gmail.com>
2025-05-14 15:08:53 -0500

    v.04

--- docs/programa.js
+++ docs/programa.js
@@ -1,10 +1,10 @@
-var map = L.map('map').setView([4.709532764012459, -74.07180212149238], 13)
+var map = L.map('map').setView([4.628108163491138, -74.06593836859484], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map)
+}).addTo(map);
 
-var marker = L.marker([4.709532764012459, -74.07180212149238]).addTo(map);
+var marker = L.marker([4.628108163491138, -74.06593836859484]).addTo(map);
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
@@ -16,3 +16,18 @@ var polygon = L.polygon([
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
+
+// Abrir archivo GeoJSON
+// JavaScript se ejecuta de tal manera que si una línea de código se demora, se pasa a la siguiente sin esperar.
+// Esto puede generar problemas cuando se requiere secuencialidad .
+
+async function cargarpuntos (){
+    var miArchivo= await fetch("microondas.geojson");
+
+    //Coinvertir el contenido a json: objeto js
+    var datos= await miArchivo.json();
+    console.log(datos);
+    console.log(datos["features"][4]);
+}
+
+cargarpuntos();
